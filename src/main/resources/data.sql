9c4419992955d5999bdf5c4c44d3e4005af0facf
harry <d5ngjoo@gmail.com>
2024-07-28 21:24:41 +0900

    update
    deleted test code

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO member (id, name) VALUES (1, 'name1')
-INSERT INTO member (id, name) VALUES (2, 'name2')
-INSERT INTO member (id, name) VALUES (3, 'name3')
-INSERT INTO member (id, name) VALUES (4, 'name4')
-INSERT INTO member (id, name) VALUES (5, 'name5')
