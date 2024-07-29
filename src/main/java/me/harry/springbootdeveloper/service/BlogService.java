a20608a08bb167bb0fe82546908986e51445ea81
harry <d5ngjoo@gmail.com>
2024-07-29 22:42:44 +0900

    [Blog] REST API > GET
    find all articles

--- src/main/java/me/harry/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/harry/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.harry.springbootdeveloper.dto.AddArticleRequest;
 import me.harry.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor    // final 이 붙거나 @NouNull 이 붙은 필드의 생성자 추가
 @Service    // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 조회 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
