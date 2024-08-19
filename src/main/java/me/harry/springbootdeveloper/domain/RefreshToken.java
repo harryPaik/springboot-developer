b7a031f3f69a0d2a83f13470a1a2417f2f571dd8
harry <d5ngjoo@gmail.com>
2024-08-19 20:58:41 +0900

    RefreshToken Domain
    realize refreshToken domain

---
+++ src/main/java/me/harry/springbootdeveloper/domain/RefreshToken.java
@@ -0,0 +1,36 @@
+package me.harry.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RefreshToken {
+
+    // 6.2.2 '엔티티 구성하기 애너테이션과 동일'
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+
+}
